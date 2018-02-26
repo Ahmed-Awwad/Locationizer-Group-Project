321590d79ce7546dcdeac7cb68a828e31104e37d
sungkwonkudo <30812602+sungkwonkudo@users.noreply.github.com>
2018-02-26 12:27:10 -0500

    MASSIVE COMMIT

---
+++ Locationizer/src/main/java/com/revature/servlet/LoginServlet.java
@@ -0,0 +1,31 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendRedirect("views/user_home.html");
+	}
+
+}
